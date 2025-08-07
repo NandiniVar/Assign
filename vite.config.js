bf746631f7f394c0fd95718506b22102b5b06b00
Nandini Varshney <92244676+NandiniVar@users.noreply.github.com>
2025-08-07 18:26:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  root: ".", // 👈 make sure this is present
+  publicDir: "public", // 👈 optional but safe
+  build: {
+    outDir: "dist",
+  },
+});
